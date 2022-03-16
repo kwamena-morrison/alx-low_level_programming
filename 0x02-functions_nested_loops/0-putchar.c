00e75ecda5448c8aab573c513903697e717d1e36
kwamena-morrison <kwamena.morrison@yahoo.com>
2022-03-16 17:52:47 +0000

    Correction of task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,16 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char str[] = "_putchar"
+		int i;
+
+	i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		++i;
+
+	}
 	_putchar('\n');
 
 	return (0);
