c5a778ea5690fa6b6cb57a527a9402950cba79e8
kwamena-morrison <kwamena.morrison@yahoo.com>
2022-03-17 03:55:37 +0000

    Corrections on file 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/**
- *
- *
- */
 
+/**
+ *print_alphabet_x10 - prints 10 times the alphabet,in lowercase,
+ *followed by a new line
+ */
 void print_alphabet_x10(void)
 {
 	char ch;
@@ -22,5 +22,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 		i++;
 	}
-
 }	
