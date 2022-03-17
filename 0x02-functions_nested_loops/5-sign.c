44c6385fb56bf785bd006af317b0103c5fc1e355
kwamena-morrison <kwamena.morrison@yahoo.com>
2022-03-17 02:00:12 +0000

    correction for task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,10 +15,13 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	} else if (n == 0)
+	
 	{
-		_putchar(0);
+		
+		_putchar(98);
 		return (0);
 	} else if (n < 0)
+	
 	{
 		_putchar('-');
 	}
