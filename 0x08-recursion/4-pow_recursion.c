5c08933357ea56a2cce0436218a4d51e211254b0
kwamena-morrison <kwamena.morrison@yahoo.com>
2022-03-31 20:54:11 +0000

    correction for task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#include "main.h"
+
 /**
  * _pow_recursion - raises x to the power of y
  * @x: Number Integer
@@ -14,5 +16,5 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-
+	return (x * _pow_recursion(x, y - 1));
 }
