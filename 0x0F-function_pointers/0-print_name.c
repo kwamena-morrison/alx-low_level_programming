cb31e38156515d4314afc866e583a81a4157aed8
kwamena-morrison <kwamena.morrison@yahoo.com>
2022-04-15 08:09:33 +0000

    correction of task 0 in intial commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,17 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
-#endif
+#include <stdio.h>
+/**
+ * print_name - Function to call a function
+ *
+ * @name: Parameter 1
+ * @f: parameter function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		if (name != NULL)
+		{
+			(*f)(name);
+		}
+	}
+}
